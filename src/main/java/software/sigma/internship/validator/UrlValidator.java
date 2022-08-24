674d2433863e24a8c0d5799901fb41e7673419de
Millrocious <vitalii.merdak.20@pnu.edu.ua>
2022-08-24 22:22:08 +0300

    change type of URL_PATTERN from string -> Pattern

--- src/main/java/software/sigma/internship/validator/UrlValidator.java
+++ src/main/java/software/sigma/internship/validator/UrlValidator.java
@@ -6,10 +6,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UrlValidator implements ConstraintValidator<Url, String> {
-    private Pattern pattern;
-    private Matcher matcher;
-    private static final String URL_PATTERN = "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]" +
-            "{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)";
+    private static final Pattern URL_PATTERN = Pattern.compile("[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]" +
+            "{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)");
 
     @Override
     public void initialize(Url constraintAnnotation) {
@@ -17,8 +15,7 @@ public class UrlValidator implements ConstraintValidator<Url, String> {
 
     @Override
     public boolean isValid(String url, ConstraintValidatorContext constraintValidatorContext) {
-        pattern = Pattern.compile(URL_PATTERN);
-        matcher = pattern.matcher(url);
+        Matcher matcher = URL_PATTERN.matcher(url);
         return matcher.matches();
     }
 }
