99f3adfb8a48e4b00055198a5a1dd85c20801f1d
Millrocious <vitalii.merdak.20@pnu.edu.ua>
2022-08-24 19:11:30 +0300

    moved url pattern to final static string

--- src/main/java/software/sigma/internship/validator/UrlValidator.java
+++ src/main/java/software/sigma/internship/validator/UrlValidator.java
@@ -2,15 +2,23 @@ package software.sigma.internship.validator;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UrlValidator implements ConstraintValidator<Url, String> {
+    private Pattern pattern;
+    private Matcher matcher;
+    private static final String URL_PATTERN = "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]" +
+            "{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)";
+
     @Override
     public void initialize(Url constraintAnnotation) {
     }
 
     @Override
-    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
-        return s.matches("[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]" +
-                "{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)");
+    public boolean isValid(String url, ConstraintValidatorContext constraintValidatorContext) {
+        pattern = Pattern.compile(URL_PATTERN);
+        matcher = pattern.matcher(url);
+        return matcher.matches();
     }
 }
