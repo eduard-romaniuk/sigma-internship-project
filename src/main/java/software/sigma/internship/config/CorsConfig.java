9ca9e321d50c790fc0b0d3b2528883010115b37b
mrzhenshen <mrzhenshen@gmail.com>
2022-08-31 01:03:51 +0300

    Fix a path pattern

--- src/main/java/software/sigma/internship/config/CorsConfig.java
+++ src/main/java/software/sigma/internship/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry
-                        .addMapping("/*")
+                        .addMapping("/**")
                         .allowedOrigins(allowedOrigin.getAllowOrigin());
             }
         };
